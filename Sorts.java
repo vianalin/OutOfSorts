8d65b3291e298ad15a5bfc997659f60a6eeca0f7
vianalin <vlin21@stuy.edu>
2020-12-16 22:26:54 -0500

    edit

--- Sorts.java
+++ Sorts.java
@@ -3,23 +3,41 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Sorts{
-  ///**Bubble sort of an int array. 
-  //*Upon completion, the elements of the array will be in increasing order.
-  //*@param data  the elements to be sorted.
-  //*/
-  public static void bubbleSort(int[] data){
-  	int dataholder = 0;
-  	boolean again = false;
-	for(int i = 0; i < data.length - 1; i++) {
-  		if(data[i] > data[i+1]) {
-  			dataholder = data[i];
-  			data[i] = data[i+1];
-  			data[i+1] = dataholder;
-  			again = true;
+
+	//public static void main(String[] args) {
+
+		//int n = 10;
+		//for (int i = 0; i < n; i++) {
+			//Random rng = new Random();
+
+			//int[] data = new int[10000];
+			//for (int a = 0; a < 10000; a++) {
+				//data[a] = rng.nextInt() * 1000;
+			//}
+
+			//int[] hold = data.clone();
+
+			//Arrays.sort(data);
+			//bubbleSort(hold);
+
+			//System.out.println(Arrays.toString(hold).equals(Arrays.toString(data)));
+		//}
+
+	//}
+
+	public static void bubbleSort(int[] data){
+  		int dataholder = 0;
+  		boolean again = true;
+		for(int i = 0; i < data.length - 1; i++) {
+  			if(data[i] > data[i+1]) {
+  				dataholder = data[i];
+  				data[i] = data[i+1];
+  				data[i+1] = dataholder;
+  				again = false;
+  			}
+  		}
+  		if(!again) {
+  		bubbleSort(data);
   		}
   	}
-  	if(again) {
-  		bubbleSort(data);
-  	}
-  }
 }
