7a060d40decc6a019abdc0555bda03ee9e123522
vianalin <vlin21@stuy.edu>
2020-12-22 19:47:53 -0500

    tested selectionsort

--- Tester.java
+++ Tester.java
@@ -5,21 +5,26 @@ import java.util.Arrays;
 public class Tester {
 	public static void main(String[] args) {
 
-		//testing bubblesort
 		ArrayList<Boolean> asort = new ArrayList<Boolean>();
 		ArrayList<Boolean> bsort = new ArrayList<Boolean>();
 		ArrayList<Boolean> csort = new ArrayList<Boolean>();
-
 		int[] a = {-9, 1, 3, 7, 9};
-		Sorts.bubbleSort(a);
+		int[] b = {78, 900, 5, 2, 8};
+		int[] c = {700, 68, 7, 4, -1};
+
+		//testing bubblesort and selection sort
+		//Sorts.bubbleSort(a);
+		Sorts.selectionSort(a);
 		System.out.println(asort.add(Arrays.toString(a).equals("[-9, 1, 3, 7, 9"))); //testing change nothing
 
-		int[] b = {78, 900, 5, 2, 8};
-		Sorts.bubbleSort(b);
+		//Sorts.bubbleSort(b);
+		Sorts.selectionSort(b);
 		System.out.println(bsort.add(Arrays.toString(b).equals("[2, 5, 8, 78,900]"))); //testing messed up order
 
-		int[] c = {700, 68, 7, 4, -1};
-		Sorts.bubbleSort(c);
+		//Sorts.bubbleSort(c);
+		Sorts.selectionSort(c);
 		System.out.println(csort.add(Arrays.toString(c).equals("[-1, 4, 7, 68, 700]"))); //testing reverse sorted
+
+
 	}
 }
